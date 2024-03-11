5100bcfa8d8c4d6cd2f5b68b766c21424a357a3d
Okechi Williams <Okechi.williams@gmail.com>
2024-03-11 16:42:40 +0000

    added randomised bot reply functionality

--- src/Events/messages/botReplies.js
+++ src/Events/messages/botReplies.js
@@ -1,31 +1,5 @@
-module.exports = (bot, message) => {
-
-    const replies = [
-        {
-            msg: "mansam",
-            msgReply: "What? Did you call me Handsome?!!",
-        },
-        {
-            msg: "throw",
-            msgReply: "Josh never threw! FACTS",
-        },
-        {
-            msg: "multi",
-            msgReply: ["one", "two", "three"],
-        },
-    ];
-    for(const reply of replies) {        
-        if(!message.author.bot && message.content === reply.msg) {
-            if(reply.msgReply.length > 1) {
-                rngReply = Math.floor(Math.random()* reply.msgReply.length);
-                message.reply({
-                    content: rngReply
-                });
-                return;
-            }
-            message.reply({
-                content: reply.msgReply
-            });
-        };
-    }
+module.exports = (bot) => {
+    bot.on('messageCreate', (message) =>{
+        console.log("I'm ready");
+    })
 };
--- src/Events/ready/botOnline.js
+++ src/Events/ready/botOnline.js
@@ -7,4 +7,41 @@ module.exports = (bot) => {
         name: "Human trial 0.3.1",
         type: ActivityType.Watching,
     });
+
+    const replies = [
+        {
+            msg: "mansam",
+            msgReply: "What? Did you call me Handsome?!!",
+        },
+        {
+            msg: "throw",
+            msgReply: "Josh never threw! FACTS",
+        },
+        {
+            msg: "multi",
+            msgReply: ["one", "two", "three"],
+        },
+    ];
+
+    bot.on('messageCreate', (message) => {
+        if (message.author.id !== process.env.CLIENT_ID) {
+            for (let i = 0; i < replies.length; i++) {
+                const reply = replies[i];
+
+                if(message.content === reply.msg) {
+                    if(Array.isArray(reply.msgReply)) {
+                        rngReply = Math.floor(Math.random()* reply.msgReply.length);
+                        message.reply({
+                            content: reply.msgReply[rngReply]
+                        });
+                        return;
+                    }else{
+                        message.reply({
+                            content: reply.msgReply
+                        });
+                    }
+                };
+            }
+        }
+    });
 };
