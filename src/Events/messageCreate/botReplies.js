e7c627d77c0708005549bd38f68e9ce21b92674c
Okechi Williams <Okechi.williams@gmail.com>
2024-03-19 16:10:02 +0000

    Moved the bot replies into its own file

--- src/Events/messageCreate/botReplies.js
+++ src/Events/messageCreate/botReplies.js
@@ -1,31 +1,23 @@
+const replyArray = require("../../Utility/replyArray");
+const prefix = "!"
 module.exports = (bot, message) => {
-    const replies = [
-        {
-            msg: "mansam",
-            msgReply: "What? Did you call me Handsome?!!",
-        },
-        {
-            msg: "throw",
-            msgReply: "Josh never threw! FACTS",
-        },
-        {
-            msg: "multi",
-            msgReply: ["one", "two", "three"],
-        },
-    ];
 
     if (message.author.id !== process.env.CLIENT_ID) {
-        for (let i = 0; i < replies.length; i++) {
-            const reply = replies[i];
+        for (let i = 0; i < replyArray.length; i++) {
+            const reply = replyArray[i];
 
-            if(message.content === reply.msg) {
+            if(message.content === prefix+reply.msg) {
+
                 if(Array.isArray(reply.msgReply)) {
+
                     rngReply = Math.floor(Math.random()* reply.msgReply.length);
                     message.reply({
                         content: reply.msgReply[rngReply]
                     });
                     return;
+
                 }else{
+
                     message.reply({
                         content: reply.msgReply
                     });
---
+++ src/Utility/replyArray.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        msg: "mansam",
+        msgReply: "What? Did you call me Handsome?!!",
+    },
+    {
+        msg: "throw",
+        msgReply: "Josh never threw! FACTS",
+    },
+    {
+        msg: "hi",
+        msgReply: [
+            "Hey! Ready for some bot banter?",
+            "Greetings! Let's chat, shall we?",
+            "Hello! Bot at your service!",
+            "Hiya! Bot's here, ready to amuse.",
+            "Hey there! Bot reporting for duty.",
+            "Salutations! Bot mode activated.",
+            "Hi! Bot's on duty. What's up?",
+            "Greetings! Bot here, standing by.",
+            "Hello! Bot's ready to entertain.",
+            "Hey! Bot's here to say hi!",
+            "Hiya! Bot's buzzing with humor.",
+            "Greetings! Bot's here to charm.",
+            "Hi! Bot's in the house!", "Hey there! Bot's in chat mode.",
+            "Salutations! Bot's in action.",
+            "Hello! Bot's here to lighten the mood.",
+            "Hi! Bot's here, armed with jokes.",
+            "Hey! Bot's ready to mingle.",
+            "Greetings! Bot's here, brightening your day.",
+            "Hiya! Bot's here, spreading joy."
+        ],
+    },
+]
